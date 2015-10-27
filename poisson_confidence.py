379b639601d3e3b4b944d3c547c22774ccbb5181
Evan Keane <e.keane@vpn2-210.its.manchester.ac.uk>
2015-10-27 15:12:27 +0000

    First commit
    First commit of a script which I will make quite a bit better.

---
+++ poisson_confidence.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import math as m
+
+# Gehrels 1986 paper http://adsabs.harvard.edu/abs/1986ApJ...303..336G
+S     = 1.645 # The Gaussian sigma level corresponding to 95% confidence
+beta  = 0.031 # Lower limit parameter appropriate for 95% confidence
+gamma = -2.50 # Lower limit parameter appropriate for 95% confidence
+
+for n in range(0,110):
+    lower = 0.0
+    # Gehrels 1986 equation 9
+    upper = (n+1)*(1.0 - (1.0/(9*(n+1.0))) + (S/(3.0*m.sqrt(n+1.0))))**3 
+    if ( n>0 ):
+        # Gehrels 1986 equation 12
+        lower = n*(1.0 - (1.0/(9.0*n)) - (S/(3.0*m.sqrt(n))) + beta*n**gamma)**3 
+    print "%.1f %d %.1f" % (upper, n,lower)
