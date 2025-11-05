5cb92fffd519c54d3deae675f0146d669430c740
Evan Ó Catháin <Evan.Keane@gmail.com>
2025-11-05 21:32:40 +0000

    Add 1sigma confidence outputs
    Add 1sigma confidence outputs for Karen.

--- poisson_confidence.py
+++ poisson_confidence.py
@@ -7,11 +7,15 @@ S     = 1.645 # The Gaussian sigma level corresponding to 95% confidence
 beta  = 0.031 # Lower limit parameter appropriate for 95% confidence
 gamma = -2.50 # Lower limit parameter appropriate for 95% confidence
 
+S     = 1.000 # The Gaussian sigma level corresponding to 84.13% confidence
+beta  = 0.0 # Lower limit parameter appropriate for 84.13% confidence
+gamma = 0.0 # Actually undefined for 84.13% confidence
+
 for n in range(0,150):
     lower = 0.0
     # Gehrels 1986 equation 9
     upper = (n+1)*(1.0 - (1.0/(9*(n+1.0))) + (S/(3.0*m.sqrt(n+1.0))))**3 
     if ( n>0 ):
-        # Gehrels 1986 equation 12
+        # Gehrels 1986 equation 14
         lower = n*(1.0 - (1.0/(9.0*n)) - (S/(3.0*m.sqrt(n))) + beta*n**gamma)**3 
     print ("%.1f %d %.1f" % (upper, n,lower))
